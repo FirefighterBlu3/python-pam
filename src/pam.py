013da9db0375c932db0ac13e1981e0615eb4c8dd
David Ford <david@blue-labs.org>
2019-11-14 10:45:59 -0500

    fix the PAM_TTY section to cleanly handle situations where we're not running atop a tty. do a lot of lint cleanups in prep for auto lint/pytest on check-in

--- src/pam.py
+++ src/pam.py
@@ -20,113 +20,133 @@ a user against the Pluggable Authentication Modules (PAM) on the system.
 Implemented using ctypes, so no compilation is necessary.
 '''
 
-__all__      = ['pam']
-__version__  = '1.8.5rc1'
-__author__   = 'David Ford <david@blue-labs.org>'
-__released__ = '2019 November 12'
-
 import os
 import sys
+import six
 
 from ctypes import CDLL, POINTER, Structure, CFUNCTYPE, cast, byref, sizeof
 from ctypes import c_void_p, c_size_t, c_char_p, c_char, c_int
 from ctypes import memmove
 from ctypes.util import find_library
 
+__all__ = ['pam']
+__version__ = '1.8.5rc2'
+__author__ = 'David Ford <david@blue-labs.org>'
+__released__ = '2019 November 14'
+
+if sys.version_info < (3, ):
+    print('WARNING, Python 2 is EOL and therefore py2 support in this '
+          "package is deprecated. It won't be actively checked for"
+          'correctness')
+
+
 class PamHandle(Structure):
     """wrapper class for pam_handle_t pointer"""
-    _fields_ = [ ("handle", c_void_p) ]
+    _fields_ = [("handle", c_void_p)]
 
     def __init__(self):
         Structure.__init__(self)
         self.handle = 0
 
+
 class PamMessage(Structure):
     """wrapper class for pam_message structure"""
-    _fields_ = [ ("msg_style", c_int), ("msg", c_char_p) ]
+    _fields_ = [("msg_style", c_int), ("msg", c_char_p)]
 
     def __repr__(self):
         return "<PamMessage %i '%s'>" % (self.msg_style, self.msg)
 
+
 class PamResponse(Structure):
     """wrapper class for pam_response structure"""
-    _fields_ = [ ("resp", c_char_p), ("resp_retcode", c_int) ]
+    _fields_ = [("resp", c_char_p), ("resp_retcode", c_int)]
 
     def __repr__(self):
         return "<PamResponse %i '%s'>" % (self.resp_retcode, self.resp)
 
-conv_func = CFUNCTYPE(c_int, c_int, POINTER(POINTER(PamMessage)), POINTER(POINTER(PamResponse)), c_void_p)
+
+conv_func = CFUNCTYPE(c_int, c_int, POINTER(POINTER(PamMessage)),
+                      POINTER(POINTER(PamResponse)), c_void_p)
+
 
 class PamConv(Structure):
     """wrapper class for pam_conv structure"""
-    _fields_ = [ ("conv", conv_func), ("appdata_ptr", c_void_p) ]
+    _fields_ = [("conv", conv_func), ("appdata_ptr", c_void_p)]
+
 
 # Various constants
-PAM_PROMPT_ECHO_OFF       = 1
-PAM_PROMPT_ECHO_ON        = 2
-PAM_ERROR_MSG             = 3
-PAM_TEXT_INFO             = 4
-PAM_REINITIALIZE_CRED     = 8
+PAM_PROMPT_ECHO_OFF = 1
+PAM_PROMPT_ECHO_ON = 2
+PAM_ERROR_MSG = 3
+PAM_TEXT_INFO = 4
+PAM_REINITIALIZE_CRED = 8
 
-PAM_TTY                   = 3
+PAM_TTY = 3
 
-libc                      = CDLL(find_library("c"))
-libpam                    = CDLL(find_library("pam"))
+libc = CDLL(find_library("c"))
+libpam = CDLL(find_library("pam"))
 
-calloc                    = libc.calloc
-calloc.restype            = c_void_p
-calloc.argtypes           = [c_size_t, c_size_t]
+calloc = libc.calloc
+calloc.restype = c_void_p
+calloc.argtypes = [c_size_t, c_size_t]
 
 # bug #6 (@NIPE-SYSTEMS), some libpam versions don't include this function
 if hasattr(libpam, 'pam_end'):
-    pam_end                   = libpam.pam_end
-    pam_end.restype           = c_int
-    pam_end.argtypes          = [PamHandle, c_int]
+    pam_end = libpam.pam_end
+    pam_end.restype = c_int
+    pam_end.argtypes = [PamHandle, c_int]
 
-pam_start                 = libpam.pam_start
-pam_start.restype         = c_int
-pam_start.argtypes        = [c_char_p, c_char_p, POINTER(PamConv), POINTER(PamHandle)]
+pam_start = libpam.pam_start
+pam_start.restype = c_int
+pam_start.argtypes = [c_char_p, c_char_p, POINTER(PamConv), POINTER(PamHandle)]
 
-pam_acct_mgmt             = libpam.pam_acct_mgmt
-pam_acct_mgmt.restype     = c_int
-pam_acct_mgmt.argtypes    = [PamHandle, c_int]
+pam_acct_mgmt = libpam.pam_acct_mgmt
+pam_acct_mgmt.restype = c_int
+pam_acct_mgmt.argtypes = [PamHandle, c_int]
 
-pam_set_item               = libpam.pam_set_item
-pam_set_item.restype       = c_int
-pam_set_item.argtypes      = [PamHandle, c_int, c_void_p]
+pam_set_item = libpam.pam_set_item
+pam_set_item.restype = c_int
+pam_set_item.argtypes = [PamHandle, c_int, c_void_p]
 
-pam_setcred               = libpam.pam_setcred
-pam_setcred.restype       = c_int
-pam_setcred.argtypes      = [PamHandle, c_int]
+pam_setcred = libpam.pam_setcred
+pam_setcred.restype = c_int
+pam_setcred.argtypes = [PamHandle, c_int]
 
-pam_strerror              = libpam.pam_strerror
-pam_strerror.restype      = c_char_p
-pam_strerror.argtypes     = [PamHandle, c_int]
+pam_strerror = libpam.pam_strerror
+pam_strerror.restype = c_char_p
+pam_strerror.argtypes = [PamHandle, c_int]
 
-pam_authenticate          = libpam.pam_authenticate
-pam_authenticate.restype  = c_int
+pam_authenticate = libpam.pam_authenticate
+pam_authenticate.restype = c_int
 pam_authenticate.argtypes = [PamHandle, c_int]
 
-class pam():
-    code   = 0
+
+class pam:
+    code = 0
     reason = None
 
     def __init__(self):
         pass
 
-    def authenticate(self, username, password, service='login', encoding='utf-8', resetcreds=True):
+    def authenticate(
+                self,
+                username: str,
+                password: str,
+                service: str = 'login',
+                encoding: str = 'utf-8',
+                resetcreds: bool = True) -> bool:
         """username and password authentication for the given service.
 
-           Returns True for success, or False for failure.
+        Returns True for success, or False for failure.
 
-           self.code (integer) and self.reason (string) are always stored and may
-           be referenced for the reason why authentication failed. 0/'Success' will
-           be stored for success.
+        self.code (integer) and self.reason (string) are always stored and may
+        be referenced for the reason why authentication failed. 0/'Success'
+        will be stored for success.
 
-           Python3 expects bytes() for ctypes inputs.  This function will make
-           necessary conversions using the supplied encoding.
+        Python3 expects bytes() for ctypes inputs.  This function will make
+        necessary conversions using the supplied encoding.
 
-        Inputs:
+        Args:
           username: username to authenticate
           password: password in plain text
           service:  PAM service to authenticate against, defaults to 'login'
@@ -153,17 +173,21 @@ class pam():
             return 0
 
         # python3 ctypes prefers bytes
-        if sys.version_info >= (3,):
-            if isinstance(username, str): username = username.encode(encoding)
-            if isinstance(password, str): password = password.encode(encoding)
-            if isinstance(service, str):  service  = service.encode(encoding)
-        else:
-            if isinstance(username, unicode):
+        if sys.version_info >= (3, ):
+            if isinstance(username, str):
                 username = username.encode(encoding)
-            if isinstance(password, unicode):
+            if isinstance(password, str):
                 password = password.encode(encoding)
-            if isinstance(service, unicode):
-                service  = service.encode(encoding)
+            if isinstance(service, str):
+                service = service.encode(encoding)
+
+        else:  # py2
+            if isinstance(username, six.text_type):
+                username = username.encode(encoding)
+            if isinstance(password, six.text_type):
+                password = password.encode(encoding)
+            if isinstance(service, six.text_type):
+                service = service.encode(encoding)
 
         if b'\x00' in username or b'\x00' in password or b'\x00' in service:
             self.code = 4  # PAM_SYSTEM_ERR in Linux-PAM
@@ -174,22 +198,38 @@ class pam():
         # anything wrong with it
         cpassword = c_char_p(password)
 
-        # if X DISPLAY is set, use it, otherwise get the STDIN tty
-        ctty = os.environ.get('DISPLAY', os.ttyname(0)).encode(encoding)
-        ctty = c_char_p(ctty)
-
         handle = PamHandle()
-        conv   = PamConv(my_conv, 0)
+        conv = PamConv(my_conv, 0)
         retval = pam_start(service, username, byref(conv), byref(handle))
 
         if retval != 0:
-            # This is not an authentication error, something has gone wrong starting up PAM
-            self.code   = retval
+            # This is not an authentication error, something has gone wrong
+            # starting up PAM
+            self.code = retval
             self.reason = "pam_start() failed"
             return False
 
-        # set the TTY, needed when pam_securetty is used and the username root is used
-        pam_set_item(handle, PAM_TTY, ctty)
+        # set the TTY, required when pam_securetty is used and the username
+        # root is used note: this is only needed WHEN the pam_securetty.so
+        # module is used; for checking /etc/securetty for allowing root
+        # logins.  if your application doesn't use a TTY or your pam setup
+        # doesn't involve pam_securetty for this auth path, don't worry
+        # about it
+        #
+        # if your app isn't authenticating root with the right password, you
+        # may not have the appropriate list of TTYs in /etc/securetty and/or
+        # the correct configuration in /etc/pam.d/*
+        #
+        # if X $DISPLAY is set, use it - otherwise if we have a STDIN tty,
+        # get it
+
+        ctty = os.environ.get('DISPLAY')
+        if not ctty and os.isatty(0):
+            ctty = os.ttyname(0)
+        if ctty:
+            ctty = c_char_p(ctty.encode(encoding))
+
+            pam_set_item(handle, PAM_TTY, ctty)
 
         retval = pam_authenticate(handle, 0)
         auth_success = retval == 0
@@ -202,7 +242,7 @@ class pam():
             retval = pam_setcred(handle, PAM_REINITIALIZE_CRED)
 
         # store information to inform the caller why we failed
-        self.code   = retval
+        self.code = retval
         self.reason = pam_strerror(handle, retval)
         if sys.version_info >= (3,):
             self.reason = self.reason.decode(encoding)
@@ -221,26 +261,32 @@ def authenticate(*vargs, **dargs):
 
 
 if __name__ == "__main__":
-    import readline, getpass
+    import readline
+    import getpass
 
     def input_with_prefill(prompt, text):
         def hook():
             readline.insert_text(text)
             readline.redisplay()
+
         readline.set_pre_input_hook(hook)
 
         if sys.version_info >= (3,):
             result = input(prompt)
+            # ignore, lint things
+            raw_input = input
         else:
             result = raw_input(prompt)
 
         readline.set_pre_input_hook()
+
         return result
 
     pam = pam()
 
     username = input_with_prefill('Username: ', getpass.getuser())
 
-    # enter a valid username and an invalid/valid password, to verify both failure and success
+    # enter a valid username and an invalid/valid password, to verify both
+    # failure and success
     pam.authenticate(username, getpass.getpass())
     print('{} {}'.format(pam.code, pam.reason))
